62679bef012671bafa250d2147f0ccbcfa1fe02d
Michael <Michael@Maik>
2017-01-26 13:27:53 +0100

    Ejercicio31Acabado

--- src/Ejercicio31/Ejercicio31.java
+++ src/Ejercicio31/Ejercicio31.java
@@ -5,6 +5,30 @@ public class Ejercicio31 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] array1 = { 1, 2, 3 };
+		int[] array2 = { 4, 5, 6, 7, 8 };
+
+		int resultado[] = concatenar(array1, array2);
+
+		for (int i = 0; i < resultado.length; i++) {
+			System.out.println(resultado[i]);
+		}
+
+	}
+
+	public static int[] concatenar(int[] array1, int[] array2) {
+
+		int arraynuevo[] = new int[array1.length + array2.length];
+
+		for (int i = 0; i < array1.length; i++) {
+			arraynuevo[i] = array1[i];
+		}
+
+		for (int j = 0; j < array2.length; j++) {
+			arraynuevo[array1.length + j] = array2[j];
+		}
+
+		return arraynuevo;
 	}
 
 }
